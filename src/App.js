b6af386ddb01c30c791f554f0830ba82010f0461
Robbie DeHaan <robbiedehaan@gmail.com>
2024-05-16 00:36:09 -0400

    migrated frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import { View, Image, useTheme } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import {
+  ShareButton,
+  SongCardCollection 
+} from './ui-components';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          src="https://github.com/robbliebob/amplofi3/blob/main/logo.png?raw=true"
+        />
+      </View>
+    );
+  },
+};
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
+      <Authenticator socialProviders={['google', 'amazon']}>
+        {({ signOut, user }) => (
+          <main>
+            <components.Header />
+            <div style={{ textAlign: 'center', flexGrow: 1 }}>
+              <ShareButton />
+              <SongCardCollection />
+            </div>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
-
-export default App;
